49d76c0549dbaf7ede02b0fe30cfa02cd7b3ecb4
Alexx <alexandar.radan@gmail.com>
2020-06-06 23:34:00 +0200

    build commit

---
+++ build/precache-manifest.f5b394b1a60c2a3b0f34b167f6a35276.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77fbb7cac439f9e1abdec0b1673b0b70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f68ea399db4c0e6d2b8",
+    "url": "/static/css/main.2a818451.chunk.css"
+  },
+  {
+    "revision": "528d3be0a05073c24f72",
+    "url": "/static/js/2.a6028b8e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a6028b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f68ea399db4c0e6d2b8",
+    "url": "/static/js/main.5355ca07.chunk.js"
+  },
+  {
+    "revision": "6d2cd4d0e332372647de",
+    "url": "/static/js/runtime-main.25e51cbb.js"
+  },
+  {
+    "revision": "bf763dc24e9d6c1abf0c13dc0051e69b",
+    "url": "/static/media/AlegreyaSans-Thin.bf763dc2.ttf"
+  },
+  {
+    "revision": "f21db5da81e02c9ae0986ea7d3416045",
+    "url": "/static/media/back.f21db5da.png"
+  },
+  {
+    "revision": "446d380d09c679c9719b0290696c3b4e",
+    "url": "/static/media/cart.446d380d.svg"
+  },
+  {
+    "revision": "eb522377beeeee9ac56397040d90f8db",
+    "url": "/static/media/city.eb522377.jpg"
+  },
+  {
+    "revision": "2bb5c0e62252a620db57e24552609d91",
+    "url": "/static/media/store.2bb5c0e6.jpg"
+  }
+]);
